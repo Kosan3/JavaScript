98da9775a09811b948d38faa1b90a0d3cabef380
Kosan3 <ichiro.neko.0421@gmail.com>
2020-08-21 21:24:57 +0900

    Dateオブジェクト

--- hello.js
+++ hello.js
@@ -1,11 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var btn = document.getElementById('btn');
-  var onoff = document.getElementById('onoff');
-  btn.addEventListener('click', function() {
-    if (onoff.checked) {
-      console.log(onoff.value);
-    } else {
-      console.log('チェックされていません');
-    }
-  }, false);
-}, false);
+var date = new Date();
+
+var year = date.getFullYear();
+var month = date.getMonth();
+var day = date.getDate();
+var weeks = date.getDay();
+
+var weekdays = ['日','月','火','水','木','金','土'];
+var weekday = weekdays[date.getDay()];
+
+var str = '今日は' + year + '年' + month + '月' + day + '日(' + weekday + ')です。';
+console.log(str);
