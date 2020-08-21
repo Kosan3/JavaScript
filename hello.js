1e914b98329529ca1fd1eb7715d7f702b6c56804
Kosan3 <ichiro.neko.0421@gmail.com>
2020-08-21 15:49:00 +0900

    イベントリスナー

--- hello.js
+++ hello.js
@@ -1,17 +1,5 @@
-class Member {
-  constructor(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-
-  getName() {
-    return this.lastName + this.firstName;
-  }
-}
-
-var member = new Member('友也', '小坂');
-console.log(member.getName());
-
-var current = new Date();
-var result = document.getElementById('result');
-result.textContent = current.toLocaleString();
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('btn').addEventListener('click', function() {
+    window.alert('ボタンがクリックされました。');
+  }, false);
+}, false);
