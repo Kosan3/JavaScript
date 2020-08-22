149675da89af01fc214dc4e505b11ac3bca17ca0
Kosan3 <ichiro.neko.0421@gmail.com>
2020-08-22 09:06:45 +0900

    Dateオブジェクト

--- hello.js
+++ hello.js
@@ -1,12 +1,10 @@
 var date = new Date();
-
+date.setDate(1);
+console.log(date);
 var year = date.getFullYear();
-var month = date.getMonth();
+var month = date.getMonth() - 3;
 var day = date.getDate();
-var weeks = date.getDay();
-
 var weekdays = ['日','月','火','水','木','金','土'];
 var weekday = weekdays[date.getDay()];
-
-var str = '今日は' + year + '年' + month + '月' + day + '日(' + weekday + ')です。';
+var str = "今日は" + year + "年" + month + '月' + day + '日(' + weekday + ')です。';
 console.log(str);
