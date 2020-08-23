a78a5bf5d9fdb6668e336ee6fc23520a6bed1258
Kosan3 <ichiro.neko.0421@gmail.com>
2020-08-23 10:37:13 +0900

    FizzBuzzプログラム

--- hello.js
+++ hello.js
@@ -1,2 +1,13 @@
-var name = '小坂';
-window.alert('こんにちは' + name + 'さん' + '\n今日はいいてんきですね');
+function fizzBuzz(x) {
+  if (x % 15 === 0) {
+    return console.log('Fizz Buzz');
+  } else if (x % 3 === 0) {
+    return console.log('Fizz');
+  } else if (x % 5 === 0) {
+    return console.log('Buzz');
+  } else {
+    console.log(x);
+  }
+}
+
+fizzBuzz(433);
