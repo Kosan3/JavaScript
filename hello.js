cd0a5d62fc7c395335739ad8efe314f08492b75b
Kosan3 <ichiro.neko.0421@gmail.com>
2020-08-24 17:57:53 +0900

    ES2015引数

--- hello.js
+++ hello.js
@@ -1,10 +1,9 @@
-var data = ['投手','捕手','内野手','外野手'];
-var players = [
-  {name:'上野', position:'捕手'},
-  {name: '坂本', position:'内野手'},
-  {name: '小坂', position:'投手'}
-];
+function sum(...nums) {
+  let result = 0;
+  for (let num of nums) {
+    result += num;
+  }
+  return result;
+}
 
-console.log(players.sort(function(x,y) {
-  return data.indexOf(x.position) - data.indexOf(y.position);
-}));
+console.log(sum(3,5,1,5,6));
