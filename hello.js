6ed1b49db074db7e8b30350a8d8496e3ff79f596
Kosan3 <ichiro.neko.0421@gmail.com>
2020-08-24 15:41:13 +0900

    Object finishe

--- hello.js
+++ hello.js
@@ -1,2 +1,10 @@
-var str = 'あいうえお';
-document.write(str.substring(1,4));
+var data = ['投手','捕手','内野手','外野手'];
+var players = [
+  {name:'上野', position:'捕手'},
+  {name: '坂本', position:'内野手'},
+  {name: '小坂', position:'投手'}
+];
+
+console.log(players.sort(function(x,y) {
+  return data.indexOf(x.position) - data.indexOf(y.position);
+}));
