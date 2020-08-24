46b0711255959778a836f303fd187ef841db1dcc
Kosan3 <ichiro.neko.0421@gmail.com>
2020-08-24 09:32:04 +0900

    java

--- hello.js
+++ hello.js
@@ -1,8 +1,2 @@
-var i = 1;
-try {
-  i *= j;
-} catch (error) {
-  console.log(error.message);
-} finally {
-  document.write('処理を完了しました。');
-}
+var str = 'あいうえお';
+document.write(str.substring(1,4));
