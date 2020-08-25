b0210b2c192e08d4bf9a57ab9cfcaa131d990ff6
Kosan3 <ichiro.neko.0421@gmail.com>
2020-08-25 10:39:11 +0900

    finish

--- hello.js
+++ hello.js
@@ -1,8 +1,5 @@
-var x = /http(s)?:\/\/([\w-]+\.)+(\/[\w- .\?%&=]*)?/g;
-var str = 'サポートサイトはhttps://localkekeibo.workです';
-str += 'こちらもお願いHTTP://www.web-dei.com';
-var results = str.match(x);
-console.log(results);
-for (var result of results) {
-  console.log(result);
-}
+var r = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/gi;
+var str1 = 'サイトはhttp://localkakeibo.workです';
+var str2 = 'サイトは「ローカルカケイボワークをです」';
+console.log(r.test(str1));
+console.log(r.test(str2));
