4242b7f64eed522a4bf4d488a281a36aeb867cd6
Kosan3 <ichiro.neko.0421@gmail.com>
2020-08-25 10:25:05 +0900

    RegExp

--- hello.js
+++ hello.js
@@ -1,2 +1,8 @@
-var str = 'あいうえお';
-document.write(str.substring(1,4));
+var x = /http(s)?:\/\/([\w-]+\.)+(\/[\w- .\?%&=]*)?/g;
+var str = 'サポートサイトはhttps://localkekeibo.workです';
+str += 'こちらもお願いHTTP://www.web-dei.com';
+var results = str.match(x);
+console.log(results);
+for (var result of results) {
+  console.log(result);
+}
