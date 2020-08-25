2a24d526c9ccf56b60a0df0a65a38c31b714567c
Kosan3 <ichiro.neko.0421@gmail.com>
2020-08-25 15:39:36 +0900

    クロージャ

--- hello.js
+++ hello.js
@@ -1,20 +1,15 @@
-function escapeHtml(str) {
-  if (!str) {
-    return '';
+function closure(init) {
+  var counter = init;
+
+  return function() {
+    return ++counter;
   }
-  str = str.replace(/&/g, '&amp;');
-  str = str.replace(/</g, '&lt;');
-  str = str.replace(/>/g, '&gt;');
-  return str;
 }
 
-function e(templates, ...values) {
-  let result = '';
-  for (let i = 0, len = templates.length; i < len; i++) {
-    result += templates[i] + escapeHtml(values[i]); // templates = `こんにちは、${name}さん！`
-  }
-  return result;
-}
+var myClosure1 = closure(1);
+var myClosure2 = closure(100);
 
-let name = '<"小坂と上野">'
-console.log(e(`こんにちは、${name}さん!`));
+console.log(myClosure1());
+console.log(myClosure2());
+console.log(myClosure1());
+console.log(myClosure2());
