d71b7d99701f5b35c65a44e06bdc18eb92b664b8
Kosan3 <ichiro.neko.0421@gmail.com>
2020-08-26 14:10:28 +0900

    プライベートメソッドの簡略的な書き方

--- hello.js
+++ hello.js
@@ -1,38 +1,38 @@
-var Triangle = function() {
+function Triangle() {
   var _base;
   var _height;
-  var checkArgs = function(val) {
-    return (typeof val === 'number' && val > 0);
-  }
 
-  this.setBase = function(base) {
-    if (checkArgs(base)) {
-      _base = base;
+  Object.defineProperties(this, {
+    base: {
+      get: function() {
+        return _base;
+      },
+      set: function(base) {
+        if(typeof base === 'number' && base > 0) {
+          _base = base;
+        }
+      }
+    },
+    height: {
+      get: function() {
+        return _height;
+      },
+      set: function(height) {
+        if(typeof height === 'number' && height > 0) {
+          _height = height;
+        }
+      }
     }
-  }
-  this.getBase = function() {
-    return _base;
-  }
-
-  this.setHeight = function(height) {
-    if (checkArgs(height)) {
-      _height = height;
-    }
-  }
-  this.getHeight = function() {
-    return _height;
-  }
-}
+  })
+};
 
 Triangle.prototype.getArea = function() {
-  return this.getBase() * this.getHeight() / 2;
-}
+  return this.base * this.height / 2;
+};
 
 var t = new Triangle();
-t._base = 10;
-t._height = 2;
-console.log(t.getArea());
-
-t.setBase(10);
-t.setHeight(2);
-console.log(t.getArea());
+t.base = 10;
+t.height = 2;
+console.log('三角形の底辺:' + t.base);
+console.log('三角形の高さ:' + t.height);
+console.log('三角形の面積:' + t.getArea());
