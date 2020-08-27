0848ca10503cdb1b76b6ab106e27efd74541d32e
Kosan3 <ichiro.neko.0421@gmail.com>
2020-08-27 10:42:00 +0900

    クラス終了

--- hello.js
+++ hello.js
@@ -1,21 +1,10 @@
-class MyIterator {
-  constructor(data) {
-    this.data = data;
+let data = {red: '赤色', yellow: '黄色'};
+let proxy = new Proxy(data, {
+  get(target, prop) {
+    console.log(prop in target)
+    return prop in target ? target[prop] :'?';
   }
+});
 
-  [Symbol.iterator]() {
-    let current = 0;
-    let that = this;
-    return {
-      next() {
-        return current < that.data.length ? {value: that.data[current++], done: false} : {done: true};
-      }
-    };
-  }
-}
-
-var ary = [0,1,2,3,4,5];
-var i2 = 0;
-for (var i = 0, len = ary.length; i < len; i++) {
-  console.log(ary[i2++]);
-}
+console.log(proxy.red);
+console.log(proxy.nhing);
