49f3f4513a66c0458c0d092cba3255f1842f6a4e
Kosan3 <ichiro.neko.0421@gmail.com>
2020-08-27 18:09:57 +0900

    6.2終了

---
+++ hello.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('btn').addEventListener('click', function() {
+    window.alert('ボタンがくりっくされました。 ');
+  }, false);
+}, false);
