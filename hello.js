6dcb963bbc153c88a093708b45334a1ce24ed90e
Kosan3 <ichiro.neko.0421@gmail.com>
2020-08-28 14:59:19 +0900

    6.4 Finish

--- hello.js
+++ hello.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('btn').addEventListener('click', function() {
-    var name = document.getElementById('name');
-    var result = document.getElementById('result');
-    result.innerHTML = 'こんにちは、' + name.value + 'さん！';
-  }, false);
-}, false);
+  document.getElementById("file").addEventListener('change', function(e) {
+    var input = document.getElementById('file').files[0];
+    var reader = new FileReader();
+    reader.addEventListener('load', function() {
+      document.getElementById('resul').textContent = reader.result;
+    }, true);
+    reader.readAsText(input, 'UTF-8');
+  }, true);
+});
