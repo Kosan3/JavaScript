6bce71426df5a3d4243a81c642333d4198b28fd6
Kosan3 <ichiro.neko.0421@gmail.com>
2020-08-30 00:24:20 +0900

    6.5 finish

--- hello.js
+++ hello.js
@@ -1,29 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var list = document.getElementById('list');
-  var del = document.getElementById('del');
-  var result = document.getElementById('result');
-
+  let list = document.getElementById('list');
+  let del = document.getElementById('del');
+  let result = document.getElementById('result');
   list.addEventListener('click', function(e) {
-      var isbn = e.target.getAttribute('data-isbn');
-      if (isbn) {
-      var img = document.createElement('img');
-      img.src = 'http://www.wings.msn.to/books/' + isbn + '/' + isbn + '.jpg';
-      img.alt = e.target.textContent;
-      img.height = 180;
-      img.width = 100;
-      if (result.getElementsByTagName('img').length > 0) {
-        result.replaceChild(img, result.firstChild);
-      } else {
+    let target = e.target;
+    let data = target.getAttribute('data-isbn');
+    if (data) {
+      let img = document.createElement('img');
+      img.src = 'http://www.wings.msn.to/books/' + data + '/' + data + '.jpg';
+      img.width = 150;
+      img.height = 200;
+      if (result.getElementsByTagName('img').length === 0) {
         result.appendChild(img);
         del.disabled = false;
+      } else {
+        result.replaceChild(img, result.firstChild);
       }
     }
   });
-  del.addEventListener('click', function() {
-    if (result.getElementsByTagName('img').length > 0) {
-      result.removeChild(result.firstChild);
-      del.disabled = true;
-    }
-  });
+  del.addEventListener('click', function(e) {
+    result.removeChild(result.firstChild);
+    del.disabled = true;
+  })
 });
-
