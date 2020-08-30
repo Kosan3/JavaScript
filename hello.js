b928c7888c736cc276eaecf0744d96167a81337d
Kosan3 <ichiro.neko.0421@gmail.com>
2020-08-30 11:12:39 +0900

    chapter6 finishe

--- hello.js
+++ hello.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-  let elem = document.getElementById('elem');
-
-  elem.addEventListener('click', function() {
-    this.classList.toggle('highlight');
-  });
-});
+  let Counter = function(elem) {
+    this.count = 0;
+    this.elem = elem;
+    elem.addEventListener('click', () => {
+      this.count++;
+      this.show();
+    });
+  };
+  Counter.prototype.show = function() {
+    console.log(this.elem.id + this.count + '回クリック');
+  }
+  let c = new Counter(document.getElementById('btn'));
+})
